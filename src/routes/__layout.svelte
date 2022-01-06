3df978bd339b9ed9f85eda4f62845e06fb3fdc23
Jascha030 <contact@jaschavanaalst.nl>
2022-01-06 21:04:12 +0100

    💄 Style dragbar

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,22 +1,43 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
+	import '../tailwind.css'
 
 	let ready: boolean = false;
 	onMount(() => (ready = true));
 </script>
 
-<div class="dragbar" />
+<!--suppress CheckEmptyScriptTag -->
+<div class="dragbar">
+	<div class='dragbar-utils'>
+
+	</div>
+	<div class='dragbar-title'>
+		<p>
+			Menu
+		</p>
+	</div>
+	<div class='dragbar-extra'>
+
+	</div>
+</div>
 
 {#if ready}
 	<slot />
 {/if}
 
-<style>
+<style lang='scss'>
 	.dragbar {
+		@apply border-b border-slate-800 grid grid-cols-3 flex align-middle items-center text-slate-200;
+
 		-webkit-app-region: drag;
 		position: absolute;
 		z-index: 100;
-		height: 40px;
+		height: 48px;
+		background: #212431;
 		width: 100%;
+
+		&-utils, &-title, &-extra {
+		  @apply flex justify-center;
+		}
 	}
 </style>
