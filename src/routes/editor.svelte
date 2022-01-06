937c897ba0143d9af30394ae30a211a49fa1f8e2
Jascha030 <contact@jaschavanaalst.nl>
2022-01-07 00:14:40 +0100

    🚧 Add editor and presentation routes.

---
+++ src/routes/editor.svelte
@@ -0,0 +1,24 @@
+<script lang='ts'>
+	import { goto } from '$app/navigation';
+</script>
+
+<div class='editor'>
+	<h1>Editor</h1>
+	<button on:click={() => goto('/')}>Menu</button>
+</div>
+
+
+<style lang='scss'>
+	.editor {
+	  @apply absolute flex text-white flex-row p-12;
+
+	  top: 48px;
+	  left:0;
+	  right:0;
+	  bottom:0;
+
+	  h1 {
+		@apply text-2xl;
+	  }
+	}
+</style>
---
+++ src/routes/presentation.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+</script>
+
+<main>
+
+</main>
+
+<style>
+
+</style>
