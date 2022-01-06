92f0c3ece5430f9eda5ccac24869c0a543f2006d
Jascha030 <contact@jaschavanaalst.nl>
2022-01-07 00:16:11 +0100

    🚧 Start Slide component.

---
+++ src/lib/components/slide/Slide.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import type SlideData from '$lib/models/slide/SlideData';
+	import { SlideLayout } from '$lib/models/slide/SlideData.js';
+
+	export let data: SlideData;
+
+	const layoutClass = data.layout === SlideLayout.Single ? 'single': 'dual';
+</script>
+
+<div class='slide-container'>
+	{#each data.content as panel}
+		<div class={'panel ' + layoutClass}>
+			ewa
+		</div>
+	{/each}
+</div>
