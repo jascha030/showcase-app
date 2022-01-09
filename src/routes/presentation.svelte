d5c7c3eedf5d20b570fc0e9e2d2d258c82a0da7e
Jascha030 <contact@jaschavanaalst.nl>
2022-01-09 03:12:26 +0100

    🚧 Started presentation component with temp slides

--- src/routes/presentation.svelte
+++ src/routes/presentation.svelte
@@ -1,10 +1,42 @@
 <script lang="ts">
+	import type SlideData, { SlidePanel } from '$lib/models/slide/SlideData';
+	import { BackgroundType, SlideLayout, SlotContentType } from '$lib/models/slide/SlideData';
+	import Slide from '$lib/components/slide/Slide.svelte';
+	import SlideContainer from '$lib/components/slide/SlideContainer.svelte';
+	import { getContext } from 'svelte';
+
+	const panel: SlidePanel = {
+		backgroundType: BackgroundType.None,
+		primary: true,
+		slot: {
+			contentType: SlotContentType.Text,
+			content: "Test 123"
+		}
+	}
+
+	const slides: SlideData[] = [
+		{
+			background: '',
+			content: [panel],
+			layout: SlideLayout.Single,
+			title: 'Test1'
+		},
+		{
+			background: '',
+			content: [panel],
+			layout: SlideLayout.Single,
+			title: 'Test2'
+		},
+	];
 </script>
 
 <main>
-
+	<SlideContainer>
+		{#each slides as slideContent, i}
+			<Slide slideData={Object.assign(slideContent, {index: getContext('SetIndex').index()})} />
+		{/each}
+	</SlideContainer>
 </main>
 
-<style>
-
+<style lang='scss'>
 </style>
